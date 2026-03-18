01a67f43c87be674c427a66c276d64763367bbec
diamengundang-ux <diamengundang@gmail.com>
2026-03-18 22:36:13 +0700

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,10 +5,14 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "select_account"
+        }
+      }
     })
   ],
   session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET
 };
-
